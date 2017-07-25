883f392aff510a35dcf5f10c409f8b765cefdccf
Dax Borges <daxborges@gmail.com>
2017-07-25 02:31:03 -0400

    - DRYed up some classes

--- src/components/SortOptions.js
+++ src/components/SortOptions.js
@@ -6,6 +6,7 @@ const SortOptions = ({ sortBy, changeSort }) => {
 
   const allClasses = 'mc--border-grey mc--border-thick mc--border-bottom ';
   const selectedClasses = 'mc--border-grey mc--border-thick mc--border-top mc--th-selected ';
+  const circleButtonClasses = 'mc--clean-button mc--circle-button mc--circle mc--vert-align-middle';
   const renderSelectClass = (sort, sortBy) => (sort === sortBy ? selectedClasses : '');
 
   return (
@@ -16,13 +17,13 @@ const SortOptions = ({ sortBy, changeSort }) => {
         <th className={ allClasses }></th>
         <th className={ allClasses }></th>
         <th className={ allClasses + renderSelectClass(sortBy, sortTypes.gold)}>
-          <button className="mc--clean-button mc--circle-button mc--circle mc--bg-gold mc--vert-align-middle" onClick={() => (changeSort(sortTypes.gold))} title={sortTypes.gold}></button>
+          <button className={'mc--bg-gold ' + circleButtonClasses} onClick={() => (changeSort(sortTypes.gold))} title={sortTypes.gold}></button>
         </th>
         <th className={ allClasses + renderSelectClass(sortBy, sortTypes.silver)}>
-          <button className="mc--clean-button mc--circle-button mc--circle mc--bg-silver mc--vert-align-middle" onClick={() => (changeSort(sortTypes.silver))} title={sortTypes.silver}></button>
+          <button className={'mc--bg-silver ' + circleButtonClasses} onClick={() => (changeSort(sortTypes.silver))} title={sortTypes.silver}></button>
         </th>
         <th className={ allClasses + renderSelectClass(sortBy, sortTypes.bronze)}>
-          <button className="mc--clean-button mc--circle-button mc--circle mc--bg-bronze mc--vert-align-middle" onClick={() => (changeSort(sortTypes.bronze))} title={sortTypes.bronze}></button>
+          <button className={'mc--bg-bronze ' + circleButtonClasses} onClick={() => (changeSort(sortTypes.bronze))} title={sortTypes.bronze}></button>
         </th>
         <th className={ allClasses + renderSelectClass(sortBy, sortTypes.total)}>
           <button className="mc--clean-button mc--bg-none mc--text-upper mc--text-dark-grey mc--text-size-1-1 mc--text-space-neg-0-5 mc--line-height-20 mc--vert-align-middle"
